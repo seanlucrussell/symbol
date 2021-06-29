bb470313343e6aa07d1d98dd7fea1b1e203195a6
Sean Russell <slrussel@rams.colostate.edu>
2021-06-28 23:03:00 -0600

    Untested changes to renderer. Trying new renderer design to take advantage of new language implementation

--- Renderer.hs
+++ Renderer.hs
@@ -11,109 +11,192 @@ import Brick.Types (Widget)
 import Brick.Widgets.Core (vBox, str, modifyDefAttr)
 import Lens.Micro
 
-class Renderer a where
-  renderDeclaration :: a -> Doc Marking -> Doc Marking -> Doc Marking
-  renderFunction :: a -> Doc Marking -> Doc Marking -> Doc Marking
-  renderCall :: a -> Doc Marking -> Doc Marking -> Doc Marking
-  renderAssignment :: a -> Doc Marking -> Doc Marking -> Doc Marking
-  renderTop :: a -> [Doc Marking] -> Doc Marking
-  renderOp :: a -> Doc Marking -> Op -> Doc Marking -> Doc Marking
-  renderFunctionType :: a -> Doc Marking -> Doc Marking -> Doc Marking
-  renderIntegerType :: a -> Doc Marking
-  renderBooleanType :: a -> Doc Marking
-  renderStringType :: a -> Doc Marking
-  renderIntegerLiteral :: a -> Doc Marking -> Doc Marking
-  renderStringLiteral :: a -> Doc Marking -> Doc Marking
-  renderBooleanLiteral :: a -> Doc Marking -> Doc Marking
-  renderVariable :: a -> Doc Marking -> Doc Marking
-  renderUnknown :: a -> Doc Marking
-  renderName :: a -> Doc Marking -> Doc Marking
+-- class Renderer a where
+--   renderDeclaration :: a -> Doc Marking -> Doc Marking -> Doc Marking
+--   renderFunction :: a -> Doc Marking -> Doc Marking -> Doc Marking
+--   renderCall :: a -> Doc Marking -> Doc Marking -> Doc Marking
+--   renderAssignment :: a -> Doc Marking -> Doc Marking -> Doc Marking
+--   renderTop :: a -> [Doc Marking] -> Doc Marking
+--   renderOp :: a -> Doc Marking -> Op -> Doc Marking -> Doc Marking
+--   renderFunctionType :: a -> Doc Marking -> Doc Marking -> Doc Marking
+--   renderIntegerType :: a -> Doc Marking
+--   renderBooleanType :: a -> Doc Marking
+--   renderStringType :: a -> Doc Marking
+--   renderIntegerLiteral :: a -> Doc Marking -> Doc Marking
+--   renderStringLiteral :: a -> Doc Marking -> Doc Marking
+--   renderBooleanLiteral :: a -> Doc Marking -> Doc Marking
+--   renderVariable :: a -> Doc Marking -> Doc Marking
+--   renderUnknown :: a -> Doc Marking
+--   renderName :: a -> Doc Marking -> Doc Marking
+-- renderTerm :: Renderer
+-- renderTerm (IdentifierTerm t) = annotate White (pretty t)
+-- renderTerm (FunctionTerm i t b) = "\\" <+> renderTerm i <+> ":" <+> renderTerm t <+> "." <+> renderTerm b
+-- renderTerm (ApplicationTerm f x) = parens (renderTerm f <+> renderTerm x)
+-- renderTerm (BooleanLiteralTerm b) = annotate Red (pretty b)
+-- renderTerm (ConditionalTerm a b c) = "if" <+> renderTerm a <+> "then" <+> renderTerm b <+> "else" <+> renderTerm c
+-- renderTerm (UnknownTerm) = "_____"
+-- renderTerm (FnTypeTerm a b) = parens (renderTerm a <+> "->" <+> renderTerm b)
+-- renderTerm (BoolTypeTerm) = annotate Yellow "Bool"
+-- renderTerm (Assignment a b) = renderTerm a <+> "=" <+> renderTerm b
 
-data SymbolRenderer = SymbolRenderer
-instance Renderer SymbolRenderer where
-  renderDeclaration _ n t = n <+> ":" <+> t
-  renderFunction _ a b = "\\" <+> a <+> "." <+> b
-  renderCall _ f a = parens (f <+> a)
-  renderAssignment _ d v = d <+> "=" <+> v
-  renderTop _ a = vsep (punctuate line a)
-  renderOp _ a op b = parens (a <+> pop <+> b)
-   where pop = case op of
-               Add -> "+"
-               Multiply -> "*"
-               Equal -> "=="
-               LessThan -> "<"
-               GreaterThan -> ">"
-               Mod -> "mod"
-               And -> "and"
-               Or -> "or"
-  renderFunctionType _ args result = group (align $ parens (args <+> "->" <+> result))
-  renderIntegerType _ = annotate Yellow $ pretty ("Nat" :: T.Text)
-  renderBooleanType _ = annotate Yellow $ pretty ("Bool" :: T.Text)
-  renderStringType _ = annotate Yellow $ pretty ("Str" :: T.Text)
-  renderIntegerLiteral _ l = annotate Red l
-  renderStringLiteral _ l = annotate Cyan ("\"" <> l <> "\"")
-  renderBooleanLiteral _ l = annotate Red l
-  renderVariable _ l = annotate White l
-  renderUnknown _ = pretty ("_____" :: T.Text)
-  renderName _ n = annotate White n
+data Renderer = Renderer { renderIdentifier :: Doc Marking -> Doc Marking
+                         , renderFunction :: Doc Marking -> Doc Marking -> Doc Marking -> Doc Marking
+                         , renderApplication :: Doc Marking -> Doc Marking -> Doc Marking
+                         , renderBooleanLiteral :: Doc Marking -> Doc Marking
+                         , renderConditional :: Doc Marking -> Doc Marking -> Doc Marking -> Doc Marking
+                         , renderUnknown :: Doc Marking
+                         , renderFnType :: Doc Marking -> Doc Marking -> Doc Marking
+                         , renderBoolType :: Doc Marking
+                         , renderAssignment :: Doc Marking -> Doc Marking -> Doc Marking
+                         , renderProgram :: [Doc Marking] -> Doc Marking }
 
-class Prettify a where
-  prettify :: Renderer b => b -> a -> Doc Marking
+basicRenderer :: Renderer 
+basicRenderer = Renderer { renderIdentifier = \t -> annotate White t
+                         , renderFunction = \i t b -> "\\" <+> i <+> ":" <+> t <+> "." <+> b
+                         , renderApplication = \f x -> parens (f <+> x)
+                         , renderBooleanLiteral = \b -> annotate Red b
+                         , renderConditional = \a b c -> "if" <+> a <+> "then" <+> b <+> "else" <+> c
+                         , renderUnknown = "_____"
+                         , renderFnType = \a b -> parens (a <+> "->" <+> b)
+                         , renderBoolType = annotate Yellow "Bool"
+                         , renderAssignment = \a b -> a <+> "=" <+> b
+                         , renderProgram = \a -> vsep (punctuate line a) }
 
-instance Prettify Name where
-  prettify r (Name n)    = renderName r (pretty n)
-  prettify r UnknownName = renderUnknown r
+renderZipper :: Renderer -> Zipper -> Doc Marking
+renderZipper r (Zipper t c) = enclose (renderTerm t) c
+        where enclose t (TopLevel a b) = renderProgram r (fmap renderTerm (reverse a) ++ [t] ++ fmap renderTerm b)
+              enclose t (FunctionArg x y c) = enclose (renderFunction r t (renderTerm x) (renderTerm y)) c
+              enclose t (FunctionArgType x y c) = enclose (renderFunction r (renderTerm x) t (renderTerm y)) c
+              enclose t (FunctionBody x y c) = enclose (renderFunction r (renderTerm x) (renderTerm y) t) c
+              enclose t (ApplicationFn x c) = enclose (renderApplication r t (renderTerm x)) c
+              enclose t (ApplicationArg x c) = enclose (renderApplication r (renderTerm x) t) c
+              enclose t (ConditionalCond x y c) = enclose (renderConditional r t (renderTerm x) (renderTerm y)) c
+              enclose t (ConditionalOptOne x y c) = enclose (renderConditional r (renderTerm x) t (renderTerm y)) c
+              enclose t (ConditionalOptTwo x y c) = enclose (renderConditional r (renderTerm x) (renderTerm y) t) c
+              enclose t (AssignmentId x c) = enclose (renderAssignment r t (renderTerm x)) c
+              enclose t (AssignmentVal x c) = enclose (renderAssignment r (renderTerm x) t) c
+              enclose t (FnTypeArg x c) = enclose (renderFnType r t (renderTerm x)) c
+              enclose t (FnTypeRet x c) = enclose (renderFnType r (renderTerm x) t) c
+              renderTerm (IdentifierTerm i) = renderIdentifier r (pretty i)
+              renderTerm (FunctionTerm a b c) = renderFunction r (renderTerm a) (renderTerm b) (renderTerm c)
+              renderTerm (ApplicationTerm a b) = renderApplication r (renderTerm a) (renderTerm b)
+              renderTerm (BooleanLiteralTerm a) = renderBooleanLiteral r (pretty a)
+              renderTerm (ConditionalTerm a b c) = renderConditional r (renderTerm a) (renderTerm b) (renderTerm c)
+              renderTerm (UnknownTerm) = renderUnknown r
+              renderTerm (FnTypeTerm a b) = renderFnType r (renderTerm a) (renderTerm b)
+              renderTerm (BoolTypeTerm) = renderBoolType r
+              renderTerm (Assignment a b) = renderAssignment r (renderTerm a) (renderTerm b)
+              renderTerm (Program a) = renderProgram r (fmap renderTerm a)
 
-instance Prettify Type where
-  prettify r (FunctionType args result) = renderFunctionType r (prettify r args) (prettify r result)
-  prettify r IntegerType                = renderIntegerType r
-  prettify r StringType                 = renderStringType r
-  prettify r BooleanType                = renderBooleanType r
-  prettify r UnknownType                = renderUnknown r
 
-instance Prettify Value where
-  prettify r (StringLiteral v)       = renderStringLiteral r  (pretty v)
-  prettify r (IntLiteral v)          = renderIntegerLiteral r     (pretty v)
-  prettify r (BooleanLiteral v)      = renderBooleanLiteral r (pretty v)
-  prettify r (Variable v)            = renderVariable r       (pretty v)
-  prettify r (Function a b)          = renderFunction r                  (prettify r a)     (prettify r b)
-  prettify r (Call f a)              = renderCall r                  (prettify r f) (prettify r a)
-  prettify r UnknownValue            = renderUnknown r
-  prettify r (BinaryOperator a op b) = renderOp r                  (prettify r a) op           (prettify r b)
+-- prettyZip :: Renderer -> Zipper -> Doc Marking
+-- prettyZip renderer z = prettyZip' z
+--  where
+--   prettyZip' (ZipperAs  t p) = encloseAs p (annotate Highlight $ prettify renderer t)
+--   prettyZip' (ZipperDec t p) = encloseDc p (annotate Highlight $ prettify renderer t)
+--   prettyZip' (ZipperVal t p) = encloseVl p (annotate Highlight $ prettify renderer t)
+--   prettyZip' (ZipperNam t p) = encloseNm p (annotate Highlight $ prettify renderer t)
+--   prettyZip' (ZipperTyp t p) = encloseTy p (annotate Highlight $ prettify renderer t)
+--   encloseNm (DeclareName t ts) n = encloseDc ts $ renderDeclaration renderer n (prettify renderer t)
+--   encloseTy (DeclareType n ts) t = encloseDc ts $ renderDeclaration renderer (prettify renderer n) t
+--   encloseTy (FnTypeArgs r ts) t = encloseTy ts $ renderFunctionType renderer t (prettify renderer r)
+--   encloseTy (FnTypeRet a ts) r = encloseTy ts $ renderFunctionType renderer (prettify renderer a) r
+--   encloseDc (FnArgs s ts) d = encloseVl ts $ renderFunction renderer d (prettify renderer s)
+--   encloseDc (AssignDecl v ts) d = encloseAs ts $ renderAssignment renderer d (prettify renderer v)
+--   encloseVl (FnBody a ts) f = encloseVl ts $ renderFunction renderer (prettify renderer a) f 
+--   encloseVl (CallName a ts) f = encloseVl ts $ renderCall renderer f (prettify renderer a)
+--   encloseVl (CallArgs f ts) v = encloseVl ts $ renderCall renderer (prettify renderer f) v
+--   encloseVl (AssignVal d ts) v = encloseAs ts $ renderAssignment renderer (prettify renderer d) v
+--   encloseVl (OpFirst op b ts) v = encloseVl ts $ renderOp renderer v op (prettify renderer b)
+--   encloseVl (OpSecond a op ts) v = encloseVl ts $ renderOp renderer (prettify renderer a) op v
+--   encloseAs (TopLevel a b) s = renderTop renderer (pJoin renderer a s b)
+-- class Prettify a where
+--   prettify :: Renderer -> a -> Doc Marking
+-- 
+-- instance Prettify Term where
+--   prettify r (Name n)    = renderName r (pretty n)
+--   prettify r UnknownName = renderUnknown r
+-- 
+-- instance Prettify Type where
+--   prettify r (FunctionType args result) = renderFunctionType r (prettify r args) (prettify r result)
+--   prettify r IntegerType                = renderIntegerType r
+--   prettify r StringType                 = renderStringType r
+--   prettify r BooleanType                = renderBooleanType r
+--   prettify r UnknownType                = renderUnknown r
+-- 
+-- instance Prettify Value where
+--   prettify r (StringLiteral v)       = renderStringLiteral r  (pretty v)
+--   prettify r (IntLiteral v)          = renderIntegerLiteral r     (pretty v)
+--   prettify r (BooleanLiteral v)      = renderBooleanLiteral r (pretty v)
+--   prettify r (Variable v)            = renderVariable r       (pretty v)
+--   prettify r (Function a b)          = renderFunction r                  (prettify r a)     (prettify r b)
+--   prettify r (Call f a)              = renderCall r                  (prettify r f) (prettify r a)
+--   prettify r UnknownValue            = renderUnknown r
+--   prettify r (BinaryOperator a op b) = renderOp r                  (prettify r a) op           (prettify r b)
+-- 
+-- instance Prettify Declare where
+--   prettify r (Declare n t) = renderDeclaration r (prettify r n) (prettify r t)
+-- 
+-- instance Prettify Assignment where
+--   prettify r (Assign d v)     = renderAssignment r (prettify r d) (prettify r v)
 
-instance Prettify Declare where
-  prettify r (Declare n t) = renderDeclaration r (prettify r n) (prettify r t)
 
-instance Prettify Assignment where
-  prettify r (Assign d v)     = renderAssignment r (prettify r d) (prettify r v)
 
-pMap :: (Renderer r, Prettify a) => r -> [a] -> [Doc Marking]
-pMap renderer = fmap (prettify renderer)
-rMap :: (Renderer r, Prettify a) => r -> [a] -> [Doc Marking]
-rMap renderer = pMap renderer . reverse
-pJoin renderer a b c = pMap renderer (reverse a) ++ [b] ++ pMap renderer c
+-- data SymbolRenderer = SymbolRenderer
+-- instance Renderer SymbolRenderer where
+--   renderDeclaration _ n t = n <+> ":" <+> t
+--   renderFunction _ a b = "\\" <+> a <+> "." <+> b
+--   renderCall _ f a = parens (f <+> a)
+--   renderAssignment _ d v = d <+> "=" <+> v
+--   renderTop _ a = vsep (punctuate line a)
+--   renderOp _ a op b = parens (a <+> pop <+> b)
+--    where pop = case op of
+--                Add -> "+"
+--                Multiply -> "*"
+--                Equal -> "=="
+--                LessThan -> "<"
+--                GreaterThan -> ">"
+--                Mod -> "mod"
+--                And -> "and"
+--                Or -> "or"
+--   renderFunctionType _ args result = group (align $ parens (args <+> "->" <+> result))
+--   renderIntegerType _ = annotate Yellow $ pretty ("Nat" :: T.Text)
+--   renderBooleanType _ = annotate Yellow $ pretty ("Bool" :: T.Text)
+--   renderStringType _ = annotate Yellow $ pretty ("Str" :: T.Text)
+--   renderIntegerLiteral _ l = annotate Red l
+--   renderStringLiteral _ l = annotate Cyan ("\"" <> l <> "\"")
+--   renderBooleanLiteral _ l = annotate Red l
+--   renderVariable _ l = annotate White l
+--   renderUnknown _ = pretty ("_____" :: T.Text)
+--   renderName _ n = annotate White n
 
-prettyZip :: Renderer r => r -> Zipper -> Doc Marking
-prettyZip renderer z = prettyZip' z
- where
-  prettyZip' (ZipperAs  t p) = encloseAs p (annotate Highlight $ prettify renderer t)
-  prettyZip' (ZipperDec t p) = encloseDc p (annotate Highlight $ prettify renderer t)
-  prettyZip' (ZipperVal t p) = encloseVl p (annotate Highlight $ prettify renderer t)
-  prettyZip' (ZipperNam t p) = encloseNm p (annotate Highlight $ prettify renderer t)
-  prettyZip' (ZipperTyp t p) = encloseTy p (annotate Highlight $ prettify renderer t)
-  encloseNm (DeclareName t ts) n = encloseDc ts $ renderDeclaration renderer n (prettify renderer t)
-  encloseTy (DeclareType n ts) t = encloseDc ts $ renderDeclaration renderer (prettify renderer n) t
-  encloseTy (FnTypeArgs r ts) t = encloseTy ts $ renderFunctionType renderer t (prettify renderer r)
-  encloseTy (FnTypeRet a ts) r = encloseTy ts $ renderFunctionType renderer (prettify renderer a) r
-  encloseDc (FnArgs s ts) d = encloseVl ts $ renderFunction renderer d (prettify renderer s)
-  encloseDc (AssignDecl v ts) d = encloseAs ts $ renderAssignment renderer d (prettify renderer v)
-  encloseVl (FnBody a ts) f = encloseVl ts $ renderFunction renderer (prettify renderer a) f 
-  encloseVl (CallName a ts) f = encloseVl ts $ renderCall renderer f (prettify renderer a)
-  encloseVl (CallArgs f ts) v = encloseVl ts $ renderCall renderer (prettify renderer f) v
-  encloseVl (AssignVal d ts) v = encloseAs ts $ renderAssignment renderer (prettify renderer d) v
-  encloseVl (OpFirst op b ts) v = encloseVl ts $ renderOp renderer v op (prettify renderer b)
-  encloseVl (OpSecond a op ts) v = encloseVl ts $ renderOp renderer (prettify renderer a) op v
-  encloseAs (TopLevel a b) s = renderTop renderer (pJoin renderer a s b)
+-- pMap :: (Renderer r, Prettify a) => r -> [a] -> [Doc Marking]
+-- pMap renderer = fmap (prettify renderer)
+-- rMap :: (Renderer r, Prettify a) => r -> [a] -> [Doc Marking]
+-- rMap renderer = pMap renderer . reverse
+-- pJoin renderer a b c = pMap renderer (reverse a) ++ [b] ++ pMap renderer c
+-- 
+-- prettyZip :: Renderer r => r -> Zipper -> Doc Marking
+-- prettyZip renderer z = prettyZip' z
+--  where
+--   prettyZip' (ZipperAs  t p) = encloseAs p (annotate Highlight $ prettify renderer t)
+--   prettyZip' (ZipperDec t p) = encloseDc p (annotate Highlight $ prettify renderer t)
+--   prettyZip' (ZipperVal t p) = encloseVl p (annotate Highlight $ prettify renderer t)
+--   prettyZip' (ZipperNam t p) = encloseNm p (annotate Highlight $ prettify renderer t)
+--   prettyZip' (ZipperTyp t p) = encloseTy p (annotate Highlight $ prettify renderer t)
+--   encloseNm (DeclareName t ts) n = encloseDc ts $ renderDeclaration renderer n (prettify renderer t)
+--   encloseTy (DeclareType n ts) t = encloseDc ts $ renderDeclaration renderer (prettify renderer n) t
+--   encloseTy (FnTypeArgs r ts) t = encloseTy ts $ renderFunctionType renderer t (prettify renderer r)
+--   encloseTy (FnTypeRet a ts) r = encloseTy ts $ renderFunctionType renderer (prettify renderer a) r
+--   encloseDc (FnArgs s ts) d = encloseVl ts $ renderFunction renderer d (prettify renderer s)
+--   encloseDc (AssignDecl v ts) d = encloseAs ts $ renderAssignment renderer d (prettify renderer v)
+--   encloseVl (FnBody a ts) f = encloseVl ts $ renderFunction renderer (prettify renderer a) f 
+--   encloseVl (CallName a ts) f = encloseVl ts $ renderCall renderer f (prettify renderer a)
+--   encloseVl (CallArgs f ts) v = encloseVl ts $ renderCall renderer (prettify renderer f) v
+--   encloseVl (AssignVal d ts) v = encloseAs ts $ renderAssignment renderer (prettify renderer d) v
+--   encloseVl (OpFirst op b ts) v = encloseVl ts $ renderOp renderer v op (prettify renderer b)
+--   encloseVl (OpSecond a op ts) v = encloseVl ts $ renderOp renderer (prettify renderer a) op v
+--   encloseAs (TopLevel a b) s = renderTop renderer (pJoin renderer a s b)
 
 data StackInstructions = StackLiteral String
                        | Push Marking
@@ -162,6 +245,6 @@ renderDoc d = Brick.Widget Brick.Fixed Brick.Fixed
       Brick.render $ vBox $ renderStack $ renderTree $ treeForm $ layoutSmart
                     (LayoutOptions (AvailablePerLine (ctx^.Brick.availWidthL) 1.0)) d)
 
-zipperToWidget :: Renderer r => r -> Zipper -> Widget ()
-zipperToWidget renderer = renderDoc . prettyZip renderer
+zipperToWidget :: Renderer -> Zipper -> Widget ()
+zipperToWidget renderer = renderDoc . renderZipper renderer
 
